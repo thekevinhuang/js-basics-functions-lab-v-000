9b9425e622e6a5dd2c9f5d4fccd292ea06808250
Kevin Huang <yellow.khaos@gmail.com>
2018-10-02 02:50:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function distanceFromHqInFeet(blockValue) {
   return footCalculator(distanceFromHqInBlocks(blockValue))
 }
 
+function distanceTravelledInFeet(blk1, blk2) {
+  return footCalculator(distanceTwoBlocks(blk1, blk2))
+}
+
 function footCalculator(blockDistance) {
   const blockSize = 264
   return blockDistance*blockSize
