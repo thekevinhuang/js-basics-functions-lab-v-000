de425bfe927a89031fe23b8dae85b3f33178f721
Kevin Huang <yellow.khaos@gmail.com>
2018-10-02 02:47:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(blockValue) {
   const hqStreet = 42
-  return Math.abs(hqStreet-blockValue)
+  return distanceTwoBlocks(hqStreet, blockvalue)
 }
 
 function distanceFromHqInFeet(blockValue) {
